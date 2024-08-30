88b954ed3f9ea66c7f1a07527ff8ad1b10fb26db
Jolkert <jolkertt@gmail.com>
2024-08-29 23:58:33 -0400

    blank project

---
+++ src/main/java/dev/jolkert/deselect/Deselect.java
@@ -0,0 +1,15 @@
+package dev.jolkert.deselect;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Deselect implements ModInitializer
+{
+	/**
+	 * Runs the mod initializer.
+	 */
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/dev/jolkert/deselect/client/DeselectClient.java
@@ -0,0 +1,15 @@
+package dev.jolkert.deselect.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeselectClient implements ClientModInitializer
+{
+	/**
+	 * Runs the mod initializer on the client environment.
+	 */
+	@Override
+	public void onInitializeClient()
+	{
+
+	}
+}
