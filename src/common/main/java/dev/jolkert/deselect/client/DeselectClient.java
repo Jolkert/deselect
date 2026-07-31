e02a7c482c202a944e6787d4c9b325f58d77ed5e
Spagles <106791090+Spagles@users.noreply.github.com>
2026-07-31 23:49:06 +1000

    Fix NPE when player is null and sync slot selection packet to server

--- src/common/main/java/dev/jolkert/deselect/client/DeselectClient.java
+++ src/common/main/java/dev/jolkert/deselect/client/DeselectClient.java
@@ -5,6 +5,7 @@ import dev.jolkert.deselect.access.DeselectAccess;
 import dev.jolkert.deselect.mixin.InventorySelectedDuck;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.protocol.game.ServerboundSetCarriedItemPacket;
 import net.minecraft.world.entity.player.Inventory;
 
 public class DeselectClient
@@ -18,9 +19,13 @@ public class DeselectClient
 
 	public static void deselectAction(Minecraft client)
 	{
+		if (client.player == null)
+		{
+			return;
+		}
+
 		while (DESELECT_KEY.consumeClick())
 		{
-			assert client.player != null;
 			Inventory inventory = client.player.getInventory();
 
 			if (((DeselectAccess) inventory).deselect$isDeselected())
@@ -34,6 +39,11 @@ public class DeselectClient
 				((DeselectAccess) inventory)
 					.deselect$setPreviousSlot(((InventorySelectedDuck) inventory).deselect$getSelected());
 				((InventorySelectedDuck) inventory).deselect$setSelected(Deselect.DESELECT_SLOT_ID);
+			}
+
+			if (client.getConnection() != null)
+			{
+				client.getConnection().send(new ServerboundSetCarriedItemPacket(((InventorySelectedDuck) inventory).deselect$getSelected()));
 			}
 		}
 	}
