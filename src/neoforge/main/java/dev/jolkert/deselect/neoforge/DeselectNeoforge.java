82f6b7e9e9438629f87c066ced9c29aa084f9c1f
jolkert <jolkertt@gmail.com>
2026-08-08 23:33:05 -0400

    made deselect key actually do something on neoforge

--- src/neoforge/main/java/dev/jolkert/deselect/neoforge/DeselectNeoforge.java
+++ src/neoforge/main/java/dev/jolkert/deselect/neoforge/DeselectNeoforge.java
@@ -1,11 +1,15 @@
 package dev.jolkert.deselect.neoforge;
 
 import dev.jolkert.deselect.Deselect;
+import dev.jolkert.deselect.client.DeselectClient;
 import dev.jolkert.deselect.client.DeselectClientLts;
+import net.minecraft.client.Minecraft;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 
 @Mod(Deselect.MOD_ID)
  @EventBusSubscriber
@@ -14,12 +18,18 @@ public class DeselectNeoforge
 	public DeselectNeoforge()
 	{
 		Deselect.init();
-		Deselect.LOGGER.info("Hello Neoforge!");
 	}
 
 	@SubscribeEvent
-	public static void onClientInit(FMLClientSetupEvent event)
+	public static void registerBindings(RegisterKeyMappingsEvent event)
 	{
 		DeselectClientLts.init();
+		event.register(DeselectClient.DESELECT_KEY);
+	}
+
+	@SubscribeEvent
+	public static void onClientTick(ClientTickEvent.Pre event)
+	{
+		DeselectClient.deselectAction(Minecraft.getInstance());
 	}
 }
